25784c9743d134bbf16ddfc66d82401870343480
dszepesv <dszepesv@scspc511.cs.uwaterloo.ca>
2014-09-20 04:21:12 -0400

    Added the first part of basic search functionality, and tests for it.

--- includes/search.php
+++ includes/search.php
@@ -2,9 +2,9 @@
 /*
 	This file contains search functions. 
 */
+require_once(__DIR__ . "/Movie.php");
 
-
-/* Perform a basic search, return an array of matching movies.
+/* Perform a basic search, return an array of matching Movies.
 
 In the basic search you can specify a set of properties of the movie. These are:
 - title
@@ -23,8 +23,41 @@ For example:
 
 */
 function basicSearch($searchString) {
+	// TODO this is not secure... check this for better options:
+	// http://www.bin-co.com/php/scripts/load/
+	$url = "http://netflixroulette.net/api/api.php?" . $searchString;
+	$contents = file_get_contents($url);
+	# now we extract the name, netflix rating and id, date
+	$moviesData = json_decode($contents);
+	$movies = array();
+	if (is_array($moviesData)) {
+		foreach ($moviesData as $movie) {
+//			var_dump($movie);
+			array_push($movies, getMovieFromNetflixData($movie));
+		}
+	} else {
+//		var_dump($moviesData);
+		array_push($movies, getMovieFromNetflixData($moviesData));
+	}
 
+	return $movies;
 }
 
 
+
+// ------------------------------------------------------ //
+// These are utility functions                            //
+// ------------------------------------------------------ //
+
+// Takes an object generated from the json response of the netflix API,
+// creates and returns a Movie object based on it.
+function getMovieFromNetflixData($movieData) {
+	return new Movie($movieData->show_title, $movieData->rating, $movieData->show_id, $movieData->release_year);
+}
+
+
+
+
+
 ?>
+
--- includes/tests/search.php
+++ includes/tests/search.php
@@ -2,14 +2,28 @@
 /*
 	This file does some tests on the search functionality.
 */
-require_once("../search.php");
+require_once(__DIR__ . "/../search.php");
+echo "Some basic query tests --------------------------------------\n";
 
-echo "The first search.. [one result]\n";
-basicSearch("title=Attack%20on%20titan");
 
-echo "\nThe second search.. [array]\n";
-basicSearch("director=Quentin%20Tarantino");
+assert("false");
 
+echo "\n[one result] ";
+$moviesTitan = basicSearch("title=Attack%20on%20titan");
+assert(count($moviesTitan) === 1);
+assert($moviesTitan[0]->mName === "Attack on Titan");
+
+echo "\n[multiple results] ";
+$moviesTarantino = basicSearch("director=Quentin%20Tarantino");
+assert(count($moviesTarantino) === 6);
+
+echo "\n[query with no results] ";
+$moviesBad = basicSearch("title=asdfjifis");
+// TODO add assertion
+
+echo "\n[conjuction query] ";
+$moviesConj = basicSearch("title=The%20Boondocks&year=2005");
+assert($moviesConj[0]->mName === "The Boondocks");
 
 
 ?>
