c81addec8545f04c48fc6235030c1e25c6e6a2a1
dszepesv <dszepesv@scspc511.cs.uwaterloo.ca>
2014-09-21 10:32:10 -0400

    Adding try-catch block.

--- ajax/search_movie.php
+++ ajax/search_movie.php
@@ -5,6 +5,7 @@ header('Content-Type: application/json');
 require_once(__DIR__ . "/../includes/search.php");
 require_once(__DIR__ . "/../includes/parseQuery.php");
 
+
 /* if started from commandline, wrap parameters to $_POST and $_GET */
 if (!isset($_SERVER["HTTP_HOST"])) {
 	parse_str($argv[1], $_GET);
@@ -15,10 +16,15 @@ $response = array("success"=>false);
 if(isset($_GET['q']))
 {
     $query = $_GET['q'];
-	$movies = Pq::parseQuery($query);
-	$response["success"] = true;
-	$response["data"] = $movies;
-//	var_dump($movies);
+	echo "$query\n";
+	try {
+		$movies = Pq::parseQuery($query);
+		print_r($movies);
+		$response["success"] = true;
+		$response["data"] = $movies;
+	} catch (Exception $e) {
+		// pass
+	}
 }
 
 echo json_encode($response);
