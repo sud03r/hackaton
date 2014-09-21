b03e25ab39de50e4eec6277c095af772f6057fda
dszepesv <dszepesv@scspc511.cs.uwaterloo.ca>
2014-09-21 01:57:49 -0400

    search preprocessing code runs "without bugs" now.

--- includes/tests/parseQuery.php
+++ includes/tests/parseQuery.php
@@ -3,6 +3,8 @@
 require_once(__DIR__ . "/../parseQuery.php");
 
 $queries = array(
+	"Scary movies rated higher than 7 on imdb"=>"mhm",
+	"movies directed by Tom Hanks with rating higher than 61% on rotten tomato"=>"this should check both critic and audiance and if matches either, report",
 	"scary movie"=>"return both the comedy and thrillers/horrors",
 	"before sunset"=>"return the movie",
 	"movies like the expandables"=>"pretty clear..",
@@ -11,16 +13,17 @@ $queries = array(
 	"quenton tarantino movies"=>"typo in his name",
 	"Hitchhiker's Guide to the Galaxy"=>"missing 'the' from the beginning",
 	"The Princess Bride"=>"easy",
-	"tarantino 2009"=>"ahh",
-	"Scary movies rated higher than 7 on imdb"=>"mhm",
-	"movies directed by Tom Hanks with rating higher than 61% on rotten tomato"=>"this should check both critic and audiance and if matches either, report"
+	"tarantino 2009"=>"ahh"
 );
 
+$iter = 0;
 foreach ($queries as $query => $descr) {
+	if ($iter > 3) break;
+	$iter += 1;
 	echo "-- query: $query\n";
 	echo "          $descr\n";
-	$movieMatching = ParseQueries::parseQuery($query);
-	var_dump($movieMatching$);
+	$movieMatching = Pq::parseQuery($query);
+	# TODO var_dump($movieMatching);
 	echo "\n";
 }
 
